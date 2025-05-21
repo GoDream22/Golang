8ad80295f4ecd5eae36626159ce93571e98f7cee
GoDream22 <lwdream22@yandex.ru>
2025-05-21 23:11:25 +0300

    dz2

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
+	fmt.Println("___Конвертер валют___")
 	const usdToEur = 0.8955
 	const usdToRub = 80.77
 	const eurTorub = usdToRub / usdToEur
+	getUserInput()
+}
+
+func getUserInput() string {
+	var inputValue string
+	fmt.Print("Введите операцию, которую хотите выполнить: ")
+	fmt.Scan(&inputValue)
+	return inputValue
+}
+
+func calculateConvert(float64, string, string) float64 {
+	var sumToConvert float64
+	var currency1 string
+	var currency2 string
 }
